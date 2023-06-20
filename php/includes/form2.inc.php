e18fe73dfb5715acd6d650e2e01e7150d2c0911f
Nalyd59 <dy.crepin@gmail.com>
2023-06-20 16:27:44 +0200

    gestion d'erreur

--- php/includes/form2.inc.php
+++ php/includes/form2.inc.php
@@ -1,63 +1,75 @@
 <form class="container h-100" action="../index.php" method="POST" enctype="multipart/form-data">
+
     <div class='row'>
+
         <section class='card col-md-7 mx-auto my-1'>
             <h2>Ajouter des données</h2>
             <div class="d-flex justify-content-center">
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <input type="text" class="form-control" placeholder="Prénom" name="firstname" required="required" pattern="[a-z]+"></div>
-                    <br><div class="form-group col-md-6">
-                        <input type="text" class="form-control" placeholder="Nom" name="lastname" required="required" pattern="[a-z]+"></div>
-                    <br><div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Prénom" name="firstname" required="required" pattern="[a-z]+">
+                    </div>
+                    <br>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Nom" name="lastname" required="required" pattern="[a-z]+">
+                    </div>
+                    <br>
+                    <div class="form-group col-md-6">
                         <label for="old">Age (18 à 70 ans)</label>
-                        <input name="old" type="number" class="form-control" placeholder="Renseignez votre age" min="18" max="70" required="required" pattern="[0-9]+"></div>
-                    <br><div class="input-group mb-3">
+                        <input name="old" type="number" class="form-control" placeholder="Renseignez votre age" min="18" max="70" required="required" pattern="[0-9]+">
+                    </div>
+                    <br>
+                    <div class="input-group mb-3">
                         <div class="input-group-prepend"><span class="input-group-text">Taille (1.26m à 3m)</span></div>
-                        <input type="number" step="0.01" class="form-control" aria-label="Somme arrondie" name ="size" min="1.26" max="3" required="required"> 
-                        <div class="input-group-append"><span class="input-group-text">cm</span></div></div>
-                    <br><div class="form-group col-md-4">
+                        <input type="number" step="0.01" class="form-control" aria-label="Somme arrondie" name="size" min="1.26" max="3" required="required">
+                        <div class="input-group-append"><span class="input-group-text">cm</span></div>
+                    </div>
+                    <br>
+                    <div class="form-group col-md-4">
                         <input name="sexe" type="radio" id="Femme" value="Mme" required="required">
                         <label for="Femme">Femme</label><br>
                         <input name="sexe" type="radio" id="Homme" value="Mr" required="required">
                         <label for="Homme">Homme</label><br>
                         <input name="sexe" type="radio" id="Autre" value="truc" required="required">
-                        <label for="Autre">Autre</label></div><br>
+                        <label for="Autre">Autre</label>
+                    </div><br>
                 </div>
             </div>
         </section>
+
         <section class='card col-md-4 mx-auto my-1'>
             <legend>Connaissances</legend>
             <div>
                 <label for="html">HTML</label>
-                <input type="checkbox" name="tricks[]" value="HTML">
+                <input type="checkbox" name="skills[]" value="HTML">
             </div>
             <div>
                 <label for="css">CSS</label>
-                <input type="checkbox" name="tricks[]" value="CSS">
+                <input type="checkbox" name="skills[]" value="CSS">
             </div>
             <div>
                 <label for="javascript">JavaScript</label>
-                <input type="checkbox" name="tricks[]" value="JAVASCRIPT">
+                <input type="checkbox" name="skills[]" value="JAVASCRIPT">
             </div>
             <div>
                 <label for="php">PHP</label>
-                <input type="checkbox" name="tricks[]" value="PHP">
+                <input type="checkbox" name="skills[]" value="PHP">
             </div>
             <div>
                 <label for="mysql">MySQL</label>
-                <input type="checkbox" name="tricks[]" value="MySQL">
+                <input type="checkbox" name="skills[]" value="MySQL">
             </div>
             <div>
                 <label for="bootstrap">Bootstrap</label>
-                <input type="checkbox" name="tricks[]" value="BOOTSTRAP">
+                <input type="checkbox" name="skills[]" value="BOOTSTRAP">
             </div>
             <div>
                 <label for="symfony">Symfony</label>
-                <input type="checkbox" name="tricks[]" value="SYMFONY">
+                <input type="checkbox" name="skills[]" value="SYMFONY">
             </div>
             <div>
                 <label for="react">React</label>
-                <input type="checkbox" name="tricks[]" value="REACT">
+                <input type="checkbox" name="skills[]" value="REACT">
             </div>
             <br>
             <div>
@@ -70,18 +82,19 @@
                 <input type="date" required="required" name='birth'>
             </div>
         </section>
+
     </div>
-    
-    <section class='card col-11 mx-auto my-1'>
+
+    <section>
         <br>
-        <label for="image"> Joindre une image (jpg ou png)</label>
+        <label for="photo"> Joindre une image (jpg ou png)</label><br>
         <input type="file" name='photo' required="required">
         <br>
     </section>
+
+    <br>
+    <br>
+
     <input name="valid" type="submit" class="btn btn-primary" value="Enregistrer les données">
+
 </form>
-
-
-
-
-
--- php/includes/ul.inc.php
+++ php/includes/ul.inc.php
@@ -1,8 +1,4 @@
 <nav>
-    <div class="list-group">
-
-    <a type="button" class="btn btn-secondary btn-lg" class="list-group-item list-group-item-action active" href="/" >Home</a>
-
     <a id='hide' type="button" class="list-group-item list-group-item-action" href="?id=debug" >Debogage</a>
     <a id='hide' type="button" class="list-group-item list-group-item-action" href="?id=concat" >Concatenation</a>
     <a id='hide' type="button" class="list-group-item list-group-item-action" href="?id=bouc" >Boucles</a>
